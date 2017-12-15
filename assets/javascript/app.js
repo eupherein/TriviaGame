723adde952c07face5748a3d8bd842c6a48ea616
Derek Bocskey <bocskey.derek@gmail.com>
2017-12-15 02:05:58 -0500

    Well, no turning back, began switching hard coded game to jQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,84 @@
-$(document).ready(function() {
-    var counter = 10;
-    function startTimer(){
-        
+    //variables    
+    var currentQuestion = 0;
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
+    var outOfTimeQuestions = 0;
+    var number = 16;
+    var intervalId;
+    var question;
+    var questionClass;
+    var choiceList;
+    var numChoices
+    var allQuestions = [{
+        question: "What was the first thing purchased through bitcoin?",
+        choices: ["xbox", "pizza", "tv", "computer"],
+        correctAnswer: 1,
+        image: "assests/images/pizza.gif"
+    },
     
-        setInterval(function() {
-          
-            counter--;
-          if (counter >= 0) {
-            span = document.getElementById("#timer");
-            span.innerHTML = counter;
-          }
-          if (counter === 0) {
-              alert('sorry, out of time');
-              clearInterval(counter);
-          }
-        }, 1000);
-      }
-      $(".btn").click(function(){
-          startTimer();
-       });
+    {
+        question: "Who created bitcoin?",
+        choices: ["Bill Gates", "Warren Buffett", "Satoshi Nakamoto", "Jamie Dimon"],
+        correctAnswer: 2,
+        image: "assests/images/anon.gif"
+    },
+    {
+        question: "What was the first bitcoin marketplace?",
+        choices: ["bitpay", "silk road", "amazon", "ebuy"],
+        correctAnswer: 1,
+        image: "assests/images/silk.gif"
+    },
+    {
+        question: "What caused the big crash of 2013?",
+        choices: ["Mt. Gox Hack", "silk Road", "Alt coin boom", "Too much mining"],
+        correctAnswer: 0,
+        image: "assests/images/hacker.gif"
+    },
+    {
+        question: "What caused the most recent crash of bitcoin that was described as a 'pop'?",
+        choices: ["Jamie Dimon FUD", "China's 'Initial Coin Offering' ban", "Not enough mining", "China banned everything again"],
+        correctAnswer: 3,
+        image: "assests/images/banned.gif"
+    },
+    ];
     
-
-
+    //hidden button to restart game
+    $("playAgain").hide();    
+ 
+    //start button that disappears when game begins 
+$("#start").click(function () {
+    $("#start").hide();
+    $(".question").html("<img src='assets/images/bitcoin/gif'/>");
+    setTimeout(function () {
+        reset();
+    }, 4000);
 });
+    //functions
+function game() {
+    $(".list-group-item").click(function() {
+        var value = $(".list-group-item").index(this);
+            console.log(value);
+            if (value === questions[currentQuestion].correct) {
+                
+            }
+    });
+    }
+
+//timer 
+function timer() {
+    intervalId = setInterval(decrement, 1000);
+
+};
+
+function decrement() {
+    $("timeLeft").html("<h3>" + number + "</h3>");
+    number--;
+    if (number === 0) {
+        stop()
+    }
+}
+
+
+
+
+
